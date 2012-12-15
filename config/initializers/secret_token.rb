62a724e69217c788e6af5a3b27ee5bd062fa8ba2
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2012-12-15 10:33:01 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+So13866283::Application.config.secret_token = '6dffb3e675eaf50f68fc0bb607e6f3d37bc3105e637512a31eaad6eb9707fca486e6b698476f642ab96c44d358b04bed1e7226fcafc0164380220134d0cd7275'
